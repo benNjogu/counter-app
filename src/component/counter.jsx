659b2f03b7e94cfd9d5fd3085b8736509be271c4
benNjogu <kamandemachafi@gmail.com>
2022-09-16 17:23:02 +0300

    Rendering lists

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -6,6 +6,7 @@ class Counter extends Component {
    */
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
@@ -13,6 +14,11 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
