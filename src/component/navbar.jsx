bdbedabb9da0d417c7f31948b49512cf87744da7
benNjogu <kamandemachafi@gmail.com>
2022-09-18 13:52:34 +0300

    Destructing arguments

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 //A stateless functional component (sfc)
-const Navbar = (props) => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a href="#" className="navbar-brand">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
